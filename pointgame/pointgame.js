273088b209e04eb1533e19bbf9a49b5335e2e8ca
TheEgglet <woodstuffthings@gmail.com>
2021-01-23 14:28:56 +0100

    May

--- pointgame/pointgame.js
+++ pointgame/pointgame.js
@@ -4,4 +4,12 @@ function increment() {
 	points.current += 1;
 	points.total += 1;
 	document.getElementById("counter").innerHTML = points.current;
+	attemptUnlock();
+}
+
+function attemptUnlock() {
+	// cursors
+	if (points.total > 14) {
+		document.getElementById("cursors").style.display = "block";
+	}
 }
