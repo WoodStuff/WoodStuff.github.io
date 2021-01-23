a6e6b5843c62548cea373ef26af5225cdbc457f8
TheEgglet <woodstuffthings@gmail.com>
2021-01-23 22:00:56 +0100

    Update pointgame.js

--- pointgame/pointgame.js
+++ pointgame/pointgame.js
@@ -1,9 +1,11 @@
 var points = {current: 0, total: 0}
+var clickPoints = {current: 0, total: 0, onCooldown: false}
 
 function increment() {
 	points.current += 1;
 	points.total += 1;
-	document.getElementById("counter").innerHTML = points.current;
+	if (points.current)
+	changeCounters();
 	attemptUnlock();
 }
 
@@ -12,4 +14,21 @@ function attemptUnlock() {
 	if (points.total > 14) {
 		document.getElementById("cursors").style.display = "block";
 	}
+}
+
+function clickPoint() {
+	points.current =- 10;
+	clickPoints.current =+ 1;
+	clickPoints.total =+ 1;
+	changeCounters();
+	document.getElementById("cpButton").disabled = true;
+	setTimeout(function() {
+		clickPoints.onCooldown = true;
+		document.getElementById("cpButton").disabled = false;
+	}, 5000)
+}
+
+function changeCounters() {
+	document.getElementById("counter").innerHTML = points.current;
+	document.getElementById("cpCounter").innerHTML = clickPoints.current;
 }
