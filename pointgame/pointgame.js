ee766d5d9f41a66c94d9fd972fd4e15ddb03a0da
TheEgglet <woodstuffthings@gmail.com>
2021-01-27 18:17:29 +0100

    Update pointgame.js

--- pointgame/pointgame.js
+++ pointgame/pointgame.js
@@ -1,35 +1,38 @@
 var points = {current: 0, total: 0}
 var clickPoints = {current: 0, total: 0, onCooldown: false}
-var cursorArray = {
-					current: 0,
-					pClick: [1, 2],
-					cost: [3, 4],
-					names: ["Normal Cursor", "Wooden Cursor"],
-					descs: ["Your average cursor.", "A stronger material for a stronger hit."]
-				  }
+var curArray = {
+		current: 0,
+		pClick: [1, 2],
+		cost: [3, 4],
+		names: ["Normal Cursor", "Wooden Cursor"],
+		descs: ["Your average cursor.", "A stronger material for a stronger hit."]
+	       }
 
+// give a point
 function increment() {
-	points.current += 1;
-	points.total += 1;
-	if (points.current)
+	points.current += curArray.pClick[curArray.current];
+	points.total += curArray.pClick[curArray.current];
+	
 	changeCounters();
 	attemptUnlock();
-	cpDisable();
+	action();
 }
 
+// attempt to unlock features of the game
 function attemptUnlock() {
 	// cursors
 	if (points.total > 14) {
 		document.getElementById("cursors").style.display = "block";
-		cpDisable();
+		action();
 	}
 	// vault
 	if (points.total > 59 && clickPoints.total > 0) {
 		document.getElementById("vault").style.display = "block";
-		updateVault();
+		action();
 	}
 }
 
+// convert 10 points to 1 click point
 function clickPoint() {
 	points.current -= 10;
 	clickPoints.current += 1;
@@ -39,14 +42,16 @@ function clickPoint() {
 	document.getElementById("cpButton").innerHTML = "+0";
 	clickPoints.onCooldown = true;
 	document.getElementById("cpButton").title = "On cooldown";
+	action();
 	setTimeout(function() {
 		clickPoints.onCooldown = false;
 		document.getElementById("cpButton").disabled = false;
-		cpDisable();
+		action();
 	}, 5000)
 }
 
-function cpDisable() {
+// things done on every action
+function action() {
 	if (points.current < 10) {
 		document.getElementById("cpButton").disabled = true;
 		document.getElementById("cpButton").innerHTML = "+0";
@@ -61,6 +66,7 @@ function cpDisable() {
 	}
 }
 
+// change the visual counters
 function changeCounters() {
 	document.getElementById("counter").innerHTML = points.current;
 	document.getElementById("cpCounter").innerHTML = clickPoints.current;
