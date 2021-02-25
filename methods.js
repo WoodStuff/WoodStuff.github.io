e9dd2fcc5135e18ee51a39601de9c6a1b649d39b
TheEgglet <woodstuffthings@gmail.com>
2021-02-25 19:48:26 +0100

    Create methods.js

---
+++ methods.js
@@ -0,0 +1,18 @@
+// METHODS (class-based)
+
+// BOOLEAN
+
+// The reverse of the boolean
+Boolean.prototype.rev = function() {
+	if (this == true) {
+		return false;
+	}
+	else {
+		return true;
+	}
+};
+
+// ---------------------------------------------------------------------------------------
+
+// FUNCTIONS (normal)
+
