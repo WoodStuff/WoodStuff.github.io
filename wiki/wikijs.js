807836e1aff232ca245be46039950819ef8d0ca4
TheEgglet <woodstuffthings@gmail.com>
2021-03-26 08:34:49 +0100

    Create wikijs.js

---
+++ wiki/wikijs.js
@@ -0,0 +1,6 @@
+function search() {
+	var meta = document.createElement('meta');
+	meta.httpEquiv = "refresh";
+	meta.content = "0; src=\"woodstuff.github.io\"";
+	document.getElementsByTagName('head')[0].appendChild(meta);
+}
