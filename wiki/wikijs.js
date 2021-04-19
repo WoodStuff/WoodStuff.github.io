43c901e7cedcb56bb644f9741b0b99f77fe1ad63
TheEgglet <woodstuffthings@gmail.com>
2021-04-19 17:59:49 +0200

    Update wikijs.js

--- wiki/wikijs.js
+++ wiki/wikijs.js
@@ -1,7 +1,5 @@
-function search() {
+function linkchange() {
 	var input = document.getElementById("search").value;
-	var meta = document.createElement('meta');
-	meta.httpEquiv = "refresh";
-	meta.content = "0; http://woodstuff.github.io/wiki/" + input;
-	document.getElementsByTagName('head')[0].appendChild(meta);
+	var search = document.getElementById("searchbutton");
+	search.href = input;
 }
