737d275a410af77fe190a2297defdaa0f4f8dd2a
WoodStuff <woodstuffthings@gmail.com>
2021-07-17 14:23:27 +0200

    config

--- geomy/stuff.js
+++ geomy/stuff.js
@@ -1,14 +1,8 @@
-/*const config = require('./config.json');
-console.log(config.TEST);*/
-
 var canvas = document.getElementById('game');
 var game = canvas.getContext('2d');
 
-game.fillStyle = 'rgb(255, 0, 0)';
-game.fillRect(0, 0, 20000, 2000);
-
-/*game.beginPath();
+game.beginPath();
 game.moveTo(100, 100);
 game.lineTo(200, 100);
 game.lineTo(100, 200);
-game.fill();*/
+game.fill();
