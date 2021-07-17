d2ea7ec50b46c2980ba871040160ce16b4e2e844
WoodStuff <woodstuffthings@gmail.com>
2021-07-17 17:09:01 +0200

    FINALLY IT FIXED

--- geomy/stuff.js
+++ geomy/stuff.js
@@ -1,22 +1,24 @@
 var canvas = document.getElementById('game');
 var game = canvas.getContext('2d');
 
+var circle = new Image();
+
 player = {
 	x: 300,
 	y: 500,
 	mass: 20,
+	tier: 'circle',
 };
 
 gameLoop = setInterval(() => {
-	game.clearRect(0, 0, canvas.width, canvas.height);
 	render();
-	player.y--;
+	player.y -= tiers[player.tier].speed;
 }, 1000 / config.FRAMERATE);
 
 function render() {
-	var circle = new Image();
 	circle.src = 'media/circle.png';
 	circle.addEventListener('load', function() {
+		game.clearRect(0, 0, canvas.width, canvas.height);
 		game.drawImage(circle, player.x, player.y, player.mass * 6.4, player.mass * 6.4);
 	}, false);
 };
---
+++ geomy/tiers.js
@@ -0,0 +1,5 @@
+const tiers = {
+	circle: {
+        speed: 3,
+    },
+}
