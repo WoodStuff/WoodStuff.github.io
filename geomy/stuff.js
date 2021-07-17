42983e74e7bd1235101c99bb0b903b0494fb257b
WoodStuff <woodstuffthings@gmail.com>
2021-07-17 16:11:01 +0200

    size

--- geomy/stuff.js
+++ geomy/stuff.js
@@ -4,10 +4,11 @@ var game = canvas.getContext('2d');
 player = {
 	x: 300,
 	y: 500,
+	mass: 20,
 };
 
 var playerimg = new Image();
 playerimg.src = 'media/circle.png';
 playerimg.addEventListener('load', function() {
-	game.drawImage(playerimg, player.x, player.y);
+	game.drawImage(playerimg, player.x, player.y, player.mass * 6.4, player.mass * 6.4);
 }, false);
