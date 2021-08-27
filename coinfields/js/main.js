e8b371bbf76899dd42603d771bf35194a9b4e666
WoodStuff <woodstuffthings@gmail.com>
2021-08-27 15:12:19 +0200

    coins per second :)

--- coinfields/js/main.js
+++ coinfields/js/main.js
@@ -10,7 +10,15 @@ function start() {
 	};
 };
 
+function updateCps() {
+	cps = new Decimal(1);
+	return cps;
+}
+
 const updateStats = setInterval(() => {
+	player.cps = updateCps();
+	player.coins = player.coins.add(player.cps.times(0.05));
+
 	document.getElementById('coins').innerHTML = formatWhole(player.coins);
 	document.getElementById('cps').innerHTML = formatWhole(player.cps);
 }, 50);
