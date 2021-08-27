bddf92685b3f1ed249a8d78aaddae1d0058d67aa
WoodStuff <woodstuffthings@gmail.com>
2021-08-27 15:55:02 +0200

    settings! yey

--- coinfields/js/settings.js
+++ coinfields/js/settings.js
@@ -2,3 +2,7 @@ function openSettings() {
 	document.getElementById('settings').style.visibility = 'visible';
 	document.getElementById('settings').style.opacity = '1';
 }
+function closeSettings() {
+	document.getElementById('settings').style.visibility = 'hidden';
+	document.getElementById('settings').style.opacity = '0';
+}
