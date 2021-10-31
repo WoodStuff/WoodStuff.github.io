6cd4fa2ce865bb969f64165de79753adf374734b
WoodStuff <woodstuffthings@gmail.com>
2021-10-31 16:10:09 +0100

    ae

---
+++ wiki/catdesc.js
@@ -0,0 +1,5 @@
+// The descriptions of categories
+const catdescs = {
+	main: 'The pages describing the main stuff of the website',
+	base: 'The main wiki functionality',
+}
--- wiki/wikijs.js
+++ wiki/wikijs.js
@@ -3,3 +3,10 @@ function linkchange() {
 	var search = document.getElementById("searchbutton");
 	search.href = input == '' ? '../index.html' : `../${input}`;
 }
+
+function specifyCategories() {
+	document.querySelectorAll('a[data-cat]').forEach(a => {
+		a.href = `category/${a.dataset.cat}`; // set the category links
+		a.title = catdescs[a.dataset.cat]; // set the description for category
+	});
+}
