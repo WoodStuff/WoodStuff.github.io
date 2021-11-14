b934bd43d5abd014ef0aeec2ebf85838fca16236
WoodStuff <woodstuffthings@gmail.com>
2021-11-14 18:19:17 +0100

    .;]]finally

--- agp/js/battle.js
+++ agp/js/battle.js
@@ -1,6 +1,7 @@
 var playerHP, playerMaxHP, playerATK, playerACCY, playerBLK;
 var enemyHP, enemyMaxHP, enemyATK, enemyACCY, enemyBLK;
 var battleTurns;
+var pastEHP, pastPHP;
 
 function fightEnemy(id) {
 	if (!player.spawner.content.includes(id)) return false;
@@ -28,7 +29,11 @@ function fightEnemy(id) {
 	document.getElementById('battle-title').innerHTML = `Vs. ${getEnemy(id).name}`;
 	document.getElementById('battle-enemy-img').src = `media/enemies/${id}.png`;
 	
-	updateBattleStats();
+	updateBattleStats(true);
+	document.getElementById('battle-player-decrease').style.transition = '0s';
+	document.getElementById('battle-enemy-decrease').style.transition = '0s';
+	document.getElementById('battle-player-decrease').style.color = '#00000000';
+	document.getElementById('battle-enemy-decrease').style.color = '#00000000';
 
 	battleTurns = setInterval(() => { battleTurn(id) }, 1200);
 	return true;
@@ -37,6 +42,8 @@ function fightEnemy(id) {
 function battleTurn(id) {
 	if (!player.inBattle) return false;
 
+	pastEHP = enemyHP;
+	pastPHP = playerHP;
 	enemyHP = enemyHP.sub(Decimal.max(playerATK.sub(enemyBLK), new Decimal(0)));
 	playerHP = playerHP.sub(Decimal.max(enemyATK.sub(playerBLK), new Decimal(0)));
 	updateBattleStats();
@@ -61,7 +68,7 @@ function battleTurn(id) {
 	
 }
 
-function updateBattleStats() {
+function updateBattleStats(first = false) {
 	document.getElementById('battle-player-hp').innerHTML = `${format(playerHP)}/${format(playerMaxHP)} HP`;
 	document.getElementById('battle-player-atk').innerHTML = `${format(playerATK)} ATK`;
 	document.getElementById('battle-player-accy').innerHTML = `${format(playerACCY)} ACCY`;
@@ -71,6 +78,25 @@ function updateBattleStats() {
 	document.getElementById('battle-enemy-atk').innerHTML = `${format(enemyATK)} ATK`;
 	document.getElementById('battle-enemy-accy').innerHTML = `${format(enemyACCY)} ACCY`;
 	document.getElementById('battle-enemy-blk').innerHTML = `${format(enemyBLK)} BLK`;
+
+	if (first) return true;
+	document.getElementById('battle-player-decrease').innerHTML = `-${pastPHP - playerHP}`;
+	document.getElementById('battle-enemy-decrease').innerHTML = `-${pastEHP - enemyHP}`;
+	document.getElementById('battle-player-decrease').style.color = '#790000';
+	document.getElementById('battle-enemy-decrease').style.color = '#790000';
+	setTimeout(decreaseStuff, 1);
+	return true;
+}
+
+function decreaseStuff() {
+	document.getElementById('battle-player-decrease').style.transition = '0.8s';
+	document.getElementById('battle-enemy-decrease').style.transition = '0.8s';
+	document.getElementById('battle-player-decrease').style.color = '#00000000';
+	document.getElementById('battle-enemy-decrease').style.color = '#00000000';
+	setTimeout(() => {
+		document.getElementById('battle-player-decrease').style.transition = '0s';
+		document.getElementById('battle-enemy-decrease').style.transition = '0s';
+	}, 501)
 	return true;
 }
 
