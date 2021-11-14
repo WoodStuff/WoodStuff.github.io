37a130daf2c039cadc3106224dc72267abac3e06
WoodStuff <woodstuffthings@gmail.com>
2021-11-14 15:45:08 +0100

    forfeiting created

--- agp/js/battle.js
+++ agp/js/battle.js
@@ -1,10 +1,19 @@
 function fightEnemy(id) {
 	if (!player.spawner.content.includes(id)) return false;
+
 	let index = player.spawner.content.indexOf(id);
 	if (index > -1) {
 		player.spawner.content.splice(index, 1);
 	}
+
 	switchTab('battle');
+	player.inBattle = true;
+
 	document.getElementById('battle-title').innerHTML = `Vs. ${getEnemy(id).name}`;
 	document.getElementById('battle-enemy-img').src = `media/enemies/${id}.png`;
+}
+
+function forfeitFight() {
+	switchTab('enemy');
+	player.inBattle = false;
 }
