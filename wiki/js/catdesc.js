7ccfcd8a33add7f99c4d7bf3ffa79ba5e347826d
WoodStuff <woodstuffthings@gmail.com>
2021-12-27 12:21:03 +0100

    cleaned up some stuff and added agp page

--- wiki/js/catdesc.js
+++ wiki/js/catdesc.js
@@ -2,4 +2,6 @@
 const catdescs = {
 	main: 'The pages describing the main stuff of the website',
 	base: 'The main wiki functionality',
+	pages: 'The website\'s pages',
+	agp: 'Everything related to AGP',
 }
