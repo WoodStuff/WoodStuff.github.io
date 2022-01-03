5205ad062818f7efcbfc6db89cf94e405cad06c7
WoodStuff <woodstuffthings@gmail.com>
2022-01-03 13:59:44 +0100

    funny stuff protection

--- agp/js/main.js
+++ agp/js/main.js
@@ -97,14 +97,15 @@ function startUpdateStats() {
 		document.getElementById('levelDisplay').innerHTML = formatWhole(player.xp.level);
 		document.getElementById('xpDisplay').innerHTML = formatWhole(player.xp.current);
 		document.getElementById('maxXpDisplay').innerHTML = formatWhole(player.xp.max);
-		player.allbuffs = player.buffs[new Decimal(0)].concat(player.buffs[1], player.buffs[2], player.buffs[3]);
 		document.getElementById('toggle-spawn-img').src = player.spawner.on ? 'media/spawner on.png' : 'media/spawner off.png';
 		document.getElementById('rbu-per-collect').innerHTML = player.rbu.collect;
 
-		player.attack = calcstats.attack();
-		player.maxhp = calcstats.maxhp();
-		player.accy = calcstats.accy();
-		player.block = calcstats.block();
+		if (!player.inBattle) {
+			player.attack = calcstats.attack();
+			player.maxhp = calcstats.maxhp();
+			player.accy = calcstats.accy();
+			player.block = calcstats.block();
+		}
 	}, 100);
 }
 
